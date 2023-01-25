96c12b3ad36c48fea8bdf7f19455d3e6c881b7cf
mohasin3639 <mohasinsayyad3639@gmail.com>
2023-01-25 12:46:27 +0530

    Added:code for validation of first name by using regex

--- src/main/java/com/bridgelabz/regex/UserRegistration.java
+++ src/main/java/com/bridgelabz/regex/UserRegistration.java
@@ -1,7 +1,27 @@
 package com.bridgelabz.regex;
-
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class UserRegistration {
-    public static void main(String[] args) {
-        System.out.println("Welcome to User Registration Program");
+        public String FirstName(String name) {
+            String regex = "[A-Z][a-z]{2,}";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(name);
+            if (matcher.matches()) {
+                return name;
+            }
+            String fail = "No match found, Please Enter correct validations for name...";
+            return fail;
+        }
+        public static void main(String[] args) {
+            System.out.println("WELCOME TO USER REGISTRATION PROGRAM USING REGEX");
+            Scanner scan = new Scanner(System.in);
+            UserRegistration obj = new UserRegistration();
+            String name;
+            System.out.println("Enter the first name, Start with capital letter and has min 3 letter :-");
+            name = scan.next();
+            String res = obj.FirstName(name);
+            System.out.println(res);
+        }
     }
-}
+
