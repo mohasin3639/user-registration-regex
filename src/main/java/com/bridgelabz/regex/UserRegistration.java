c1017240f9c6457b6f24c7a57625fa6252686708
mohasin3639 <mohasinsayyad3639@gmail.com>
2023-01-25 17:24:46 +0530

    ADDED:code for validation of Email Id by using regex

--- src/main/java/com/bridgelabz/regex/UserRegistration.java
+++ src/main/java/com/bridgelabz/regex/UserRegistration.java
@@ -13,6 +13,16 @@ public class UserRegistration {
             String fail = "No match found, Please Enter correct validations for name...";
             return fail;
         }
+    public String email(String email){
+        String regex = "[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        if(matcher.matches()){
+            return email;
+        }
+        String fail = "No match found, Please Enter correct validations for Email...";
+        return fail;
+    }
         public static void main(String[] args) {
             System.out.println("WELCOME TO USER REGISTRATION PROGRAM USING REGEX");
             Scanner scan = new Scanner(System.in);
@@ -29,6 +39,12 @@ public class UserRegistration {
             lastName = scan.next();
             String lastname = obj.FirstName(lastName);
             System.out.println("Last Name is:- "+lastName);
+
+            String emailId;
+            System.out.println("Enter your email Id");
+            emailId = scan.next();
+            String email = obj.email(emailId);
+            System.out.println("Email Id is :- "+email);
         }
     }
 
